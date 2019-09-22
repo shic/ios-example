8b68a65051015af49dbda754f74f1d03c6e99fd6
shi <itshilei@gmail.com>
2019-09-22 17:03:35 +0200

    first tableview

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -8,7 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "This is the cell for \(indexPath.row)"
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
