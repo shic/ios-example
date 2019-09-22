20e306d562177e36f2fa4dff4f4dd65039a8e05a
shi <itshilei@gmail.com>
2019-09-22 16:42:54 +0200

    Init project with ios 12.4

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by UperProgrammer on 22/09/2019.
+//  Copyright © 2019 Uper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
